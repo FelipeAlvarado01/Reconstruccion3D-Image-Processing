464416593b64b9043d37b5b3e42e49b24fd8dee5
Andres Felipe Alvarado Vera <est.andres.alvarado@unimilitar.edu.co>
2021-10-08 17:53:20 -0500

    Creacion del main, filtros para una imagen y creacion de binarizacion para bordes

--- Scripts/Filtros.m
+++ Scripts/Filtros.m
@@ -1,11 +1,70 @@
-function R = Filtros(I)
 
-%I = imread('PruebaImg_3.jpg')
 
-%%R = ((I-0)/(225-0))*255;
-%%imshow(R)
-%info = dicominfo ( '000097.dcm' );
-%Y = dicomread (info);
+I = dicomread('1-113.dcm');
 
+%I = imread('Pinguinos.jpg');
+Idouble = double(I(:,:,1));
+Imax = max(max(Idouble))
+s = size(I);
+
+%%Prueba de contraste
+C = Idouble* 2;
+
+%%Pruebas convolucion con filtros binomial
+Sua = [1 2 1; 2 4 2; 1 2 1]/9;
+%Bin4x4 = [1 3 3 1; 3 9 9 3; 3 9 9 3; 1 3 3 1]/8;
+Bin5x5 = [1 4 6 4 1; 4 16 24 16 4; 6 24 36 24 6; 4 16 24 16 4; 1 4 6 4 1]/256;
+%Bin6x6 = [1 5 10 10 5 1;5 25 50 50 25 5; 10 50 100 100 50 10; 10 50 100 100 50 10; 5 25 50 50 25 5; 1 5 10 10 5 1]/32; 
+
+p = imread('cameraman.tif');
+BconvBin = conv2(Bin5x5,C);
+sizeB = size(BconvBin);
+M = uint16(BconvBin(3:sizeB(1)-2,3:sizeB(2)-2));
+
+H = imhist(M,65536);
+%stem(H);
+
+%%BconvSua = conv2(Sua,BconvBin); 
+%%kernel = [1 1 1; 1 1 1; 1 1 1]/9; 
+
+%%pasaAltos = Idouble - BconvSua;
+  
+%Convolucion manual
+%{
+for i = 3:s(1)-2
+    for j=3:s(2)-2
+        ventana = C(i-2:i+2,j-2:j+2);
+        prod = ventana .* Bin;
+        pix = sum(sum(prod));
+        B(i,j) = pix;
+    end    
+end
+%}
+
+%Binarizacion nos ayuda a quitar defectos de la foto como el objeto en el
+%que se postra la cabeza
+
+
+T=zeros(size(BconvBin));
+
+U = uint16(otsuthresh(H)*65536);
+
+for i = 1: sizeB(1)
+    for j=1: sizeB(2)
+        if BconvBin(i,j,1) < 3000
+            T(i,j,1) = 0;
+        else
+            T(i,j,1) = 255;
+        end    
+    end
 end
 
+max(max(BconvBin))
+
+figure(1),subplot(2,2,1),imshow(uint16(Idouble),[]),title('Imagen original') 
+figure(1),subplot(2,2,2),imshow(uint16(BconvBin),[]),title('Imagen binomial 5x5')
+figure(1),subplot(2,2,3),imshow(uint8(T)),title('Imagen binarizacion')
+imtool(I);
+
+    
+   
--- Scripts/Main.m
+++ Scripts/Main.m
@@ -9,38 +9,47 @@ h = waitbar(0,'leyendo Archivos....');
 
 %Ciclo de carga de pila de imagenes
 for i=1 :  size(No_imgs,1)
-   I(:,:,i)= dicomread(No_imgs(i).name);
-   %figure(1)
-   %imshow(I(:,:,i));
-   %pause(0.2) %Reproducir con menor velocidad las imagnes
+   I(:,:,i)= dicomread(No_imgs(i).name); %I(m,n,#imagen)
+   figure(1)
+   imshow(I(:,:,i),[]);
+    pause(0.2) %Reproducir con menor velocidad las imagnes
    waitbar(i/size(No_imgs,1)); 
 end
 close(h); 
 
 
 %Filtro #1_ contraste R=I*K  K[0,2]
-for i=1 :  size(No_imgs,1)
+
+%{
+ and for i=1 :  size(No_imgs,1)
    R(:,:,i) = I(:,:,i)+32765;
    figure(1)
-   %imshow(I(:,:,i));
+   imshow(I(:,:,i));
    figure(2)
-   %imshow(R(:,:,i));
-end
+   imshow(R(:,:,i));
+   end
+ %}
 
-%Filtro #2_Binarizacion
-a = [1;4;6;4;1];
-b = [1 4 6 4 1];
+%Filtro #2_Binomial
+a = [1;4;6;4;1]/16;
+b = [1 4 6 4 1]/16;
+%f_binomial = 1/16*(b*a);
 
-f_binomial = 1/16*(b*a);
+%Conevtir las imagenes a double
+%{
+Idouble = double(I);
+T = Idouble*0;
+Bin = [1 4 6 4 1; 4 16 24 16 4; 6 24 36 24 6; 4 16 24 16 4;1 4 6 4 1]/16;
+
 
 %for i=1 :  size(No_imgs,1)
-   T(:,:,1) = convmtx2(double (I(:,:,1)),[a b]); 
-   figure(1)
-   imshow(I(:,:,1));
+   T = conv2(Bin,I); 
+   figure(1) 
+   imshow(I);
    figure(2)
-   imshow(T(:,:,1));
+   imshow(T);
 %end
-
+%}
 
 
 
